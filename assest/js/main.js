4d3764fae6d3ee2d0d1ca4d3c654e12f0ff9ee0b
masoo <m.goudarzi8933@gmail.com>
2024-02-14 22:27:28 +0330

    update main.js

--- assest/js/main.js
+++ assest/js/main.js
@@ -35,14 +35,88 @@ $(document).ready(function () {
 
 
 
+    // scroll
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if (scroll >= 50) {
+            $('.sticky').addClass('stickyadd');
+        } else {
+            $('.sticky').removeClass('stickyadd');
+        }
+    });
+
+    var $child = $('.way-fade-up').children();
+    $child.each(function () {
+        var self = $(this);
+        $(this).waypoint(function () {
+            self.addClass('animated fadeInUp');
+        }, { offset: '90%' });
+    });
+
+    var $child = $('.way-fade-left').children();
+    $child.each(function () {
+        var self = $(this);
+        $(this).waypoint(function () {
+            self.addClass('animated fadeInLeft');
+        }, { offset: '90%' });
+    });
+
+    var $child = $('.way-fade-right').children();
+    $child.each(function () {
+        var self = $(this);
+        $(this).waypoint(function () {
+            self.addClass('animated fadeInRight');
+        }, { offset: '90%' });
+    });
+
+    // type animated
+    var typed = new Typed(".element", {
+        strings: ["easy", "High quality", "BUY"],
+        smartBackspace: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
+
+    // progress bars
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function () {
+            var p = document.querySelectorAll('.progress-bar');
+
+            p[0].setAttribute('style', 'width:100%;transition:all 1s;');
+            p[1].setAttribute('style', 'width:100%;transition:all 1.5s;');
+            p[2].setAttribute('style', 'width:100%;transition:all 1.7s;');
+            p[3].setAttribute('style', 'width:100%;transition:all 2s;');
+            p[4].setAttribute('style', 'width:100%;transition:all 2.3s;');
+        },
+        offset: '100%'
+    });
+
+    // filter work
+    var filterizd = $('.filter-container').filterizr({
+        animationDuration: .5,
+        // layout: 'horizontal',
+    });
 
 
 
+    // owl carousel
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        items: 1,
+        animateIn: "fadeInRight"
+    });
 
-
-
-
-
-
+    $('a').smoothScroll({
+        speed: 1000,
+    });
 
 });
